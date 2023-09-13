7fda18b56cbaff53cea0fcf91299779bc71fb7f5
Danish Siddiqui <139640872+danishsiddiqui994@users.noreply.github.com>
2023-09-13 17:12:48 +0500

    JavaScript assignment

---
+++ JavaScript Assignment 17-20/app.js
@@ -0,0 +1,121 @@
+// question 1
+
+// let arrayofArrays = [ [],[],[] ];
+
+// question2 
+
+// var multiDimentionalArrays = [ [0,1,2,3],[1,0,1,2],[2,1,0,1] ];
+
+// document.write(multiDimentionalArrays[0]+"<br>"+ multiDimentionalArrays[1]+"<br>"+multiDimentionalArrays[2]+"<br>")
+
+
+// question 3
+
+// for (i=1; i<=10; i++){
+// document.write(i,"<br>")
+
+// }
+
+// question 4
+
+// var multiplyTable = +prompt("Enter a number to show its multiplication table");
+// var tableLength = +prompt("Enter length multiplication table");
+
+// for (var i=1; i<=tableLength ;i++){
+//     document.write(multiplyTable + "x"+ i + " = "+multiplyTable*i + "<br>")
+// }
+
+// question 5
+
+// var fruits = [ "apple" , "banana" , "mango" , "orange" , "stawbery"]
+
+// for( var i=0; i<fruits.length; i++){
+//     document.write(fruits[i]+"<br>");
+// }
+// for(var i=0; i<fruits.length; i++){
+//     document.write(" Element at index "+ i + " is " + fruits [i] + " <br> ")
+// }
+
+
+// question 6
+// 6a
+
+// document.write("<h3>Counting:<h3>");
+// for(let i=1; i<=15; i++){
+//     document.write(i + ",")
+
+//     }
+//     6b
+
+//     document.write("<h3>Reverse counting:<h3>");
+// for(let i=10; i>=1; i--){
+//     document.write(i + ",")
+
+//     }
+// 6c
+    // document.write("<h3>Even:<h3>");
+
+    // for(let i-0; i<-20; i+-2){
+    //     document.write(i + ",")
+    
+    //     }
+
+    // 6d
+
+    // document.write("<h3>Odd:<h3>");
+
+    // for(let i=1; i< 20; i+=2){
+    //     document.write(i + ",")
+    
+    //     }
+
+    // 6e
+
+    // document.write("<h3>Series:<h3>");
+
+    // for(i- 2; i<- 20; i+-2){
+    //     document.write(i + " k " + " , ")
+
+    // }
+
+    
+
+// question 7 
+
+// let bakeryItems = ["cake" , "apple pie" , "cookie" ,"chips" ,"patties"];
+// let userInput = prompt( "Welcome to ABC Bakery.What do you want to order sir/maam?");
+// let matchfoud = false
+
+// for(let i=0 ; i<bakeryItems.length; i++){
+
+//     if( bakeryItems[i] == userInput){
+//         matchfoud = true;
+//         itemIndex = i
+
+//     }
+//    }
+//    if(matchfoud) {
+//     alert(userInput +" is available at index " +itemIndex+" in our bakery");
+// }else{
+//     alert("We are sorry. "+ userInput+ " is not available in our bakery.")
+// }
+
+
+// question 8
+
+
+// let largestNumber = [24, 53, 78, 91, 12];
+// let findLargestNum = largestNumber[0];
+
+// for(let i=0; i<largestNumber.length; i++){
+
+//     if(largestNumber[i]>findLargestNum){
+//         findLargestNum=largestNumber[i]
+//     }
+
+//     document.write("Arrays items: "+largestNumber+"<br>The largest number is " + findLargestNum);
+// }
+
+// question 9
+
+
